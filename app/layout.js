90c41484dcc07eb4f5ef60028c822483abb503f1
dhlittl <dhlittl@clemson.edu>
2025-02-20 08:35:00 -0500

    Initial commit

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,7 @@
-'use client';
-import { Amplify } from 'aws-amplify';
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-
-Amplify.configure({
-  Auth: {
-    region: 'us-east-1',
-    // other auth config will be set through env variables in Amplify Console
-  }
-});
-
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <Authenticator>
-          {({ signOut, user }) => (
-            <main>
-              {children}
-            </main>
-          )}
-        </Authenticator>
-      </body>
-    </html>
-  );
-}
+    return (
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    )
+  }
