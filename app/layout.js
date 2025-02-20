94fca2ac496d7662680f96e299dc49cad70e0978
dhlittl <dhlittl@clemson.edu>
2025-02-20 08:23:28 -0500

    Testing Gen 1

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,27 @@
-export default function RootLayout({ children }) {
-    return (
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    )
+'use client';
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure({
+  Auth: {
+    region: 'us-east-1',
+    // other auth config will be set through env variables in Amplify Console
   }
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Authenticator>
+          {({ signOut, user }) => (
+            <main>
+              {children}
+            </main>
+          )}
+        </Authenticator>
+      </body>
+    </html>
+  );
+}
