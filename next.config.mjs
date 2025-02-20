1f3a0f4f096809ef9c20201f9835d1c062cafb37
dhlittl <dhlittl@clemson.edu>
2025-02-20 07:41:49 -0500

    Copied Jeremy's Amplify test and replaced current code with that, as that was proven to work

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'standalone',
-    distDir: '.next',
-    // Fix assetPrefix to use proper URL format
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/' : ''
-  };
-  
-  export default nextConfig;
