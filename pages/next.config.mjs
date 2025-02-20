8b89a5d7dc862a9a35fc0ceb6eef15001cc3c069
dhlittl <dhlittl@clemson.edu>
2025-02-20 06:31:58 -0500

    Added console logs

--- pages/next.config.mjs
+++ pages/next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
+console.log('Loading next.config.mjs');
 const nextConfig = {
-    output: 'standalone',
-    basePath: '',
-    distDir: '.next',
-  };
-  
-  export default nextConfig;
+  output: 'standalone',
+  distDir: '.next',
+  // Log the environment
+  onDevelopmentStart: async () => {
+    console.log('Development server starting');
+  },
+  onBuildStart: async () => {
+    console.log('Build starting');
+  }
+};
+console.log('Next config:', nextConfig);
+export default nextConfig;
